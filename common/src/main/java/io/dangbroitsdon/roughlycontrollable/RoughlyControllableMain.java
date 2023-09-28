85fcbaca00f504bb6f491736616efa7e9c2be152
Donavan Campbell <65779562+dangbroitsdon@users.noreply.github.com>
2023-09-27 22:05:55 -0400

    fix leftover files from old version (1.0.0)
    hey:)

--- common/src/main/java/io/dangbroitsdon/roughlycontrollable/RoughlyControllableMain.java
+++
@@ -1,9 +0,0 @@
-package io.dangbroitsdon.roughlycontrollable;
-
-public class RoughlyControllableMain {
-    public static final String MOD_ID = "roughlycontrollable";
-
-    public static void init() {
-        System.out.println(RoughlyControllableExpectPlatform.getConfigDirectory().toAbsolutePath().normalize().toString());
-    }
-}
--- fabric-like/src/main/java/io/dangbroitsdon/roughlycontrollable/fabriclike/RoughlyControllableFabricLikeMain.java
+++
@@ -1,9 +0,0 @@
-package io.dangbroitsdon.roughlycontrollable.fabriclike;
-
-import io.dangbroitsdon.roughlycontrollable.RoughlyControllableMain;
-
-public class RoughlyControllableFabricLikeMain {
-    public static void init() {
-        RoughlyControllableMain.init();
-    }
-}
